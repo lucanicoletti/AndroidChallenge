5e707c91f65fece36fe5ab9c0103ec736427b0e4
Luca Nicoletti <luca.nicolett@gmail.com>
2019-04-13 21:01:06 +0100

    Get rid of dummy test function for Zip

--- domain/src/main/java/com/example/domain/usecases/CommentsUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/CommentsUseCase.kt
@@ -1,25 +1,13 @@
 package com.example.domain.usecases
 
-import android.annotation.SuppressLint
 import com.example.domain.models.CommentDomainModel
 import com.example.domain.repositories.CommentsRepository
 import io.reactivex.Single
-import io.reactivex.functions.BiFunction
-import io.reactivex.functions.Function3
 import javax.inject.Inject
 
 class CommentsUseCase @Inject constructor(private val commentsRepository: CommentsRepository)
     : SingleUseCase<List<CommentDomainModel>, Any>() {
 
     override fun buildUseCaseSingle(params: Any): Single<List<CommentDomainModel>> =
-        commentsRepository.getComments()
-
-    @SuppressLint("CheckResult")
-    private fun test() {
-        Single.zip(
-                commentsRepository.getComments(),
-                commentsRepository.getComments(),
-                commentsRepository.getComments(),
-                Function3<List<CommentDomainModel>, List<CommentDomainModel>, List<CommentDomainModel>, Any> { t1, t2, t3 -> })
-    }
+            commentsRepository.getComments()
 }
