b0d31f01ddb8af061e0265364c7465d0be2b4191
Luca Nicoletti <luca.nicolett@gmail.com>
2021-11-11 12:43:40 +0000

    remove Rx dependencies

--- app/src/main/java/com/lnicolet/androidchallenge/di/modules/NetworkModule.kt
+++ app/src/main/java/com/lnicolet/androidchallenge/di/modules/NetworkModule.kt
@@ -6,7 +6,6 @@ import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
@@ -25,7 +24,6 @@ class NetworkModule {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(okHttpClient)
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
